6db6f3e8e4db450876269f3282d69024443e4b7c
laancee <151016462+laancee@users.noreply.github.com>
2026-03-19 16:36:27 +0800

    Fix Sidebar casing mismatch

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 // NOTE: Make sure these paths & casing match your actual files:
 import ProductCard from '../components/productCard';
-import Sidebar from '../components/Sidebar';
+import Sidebar from '../components/SideBar';
 
 import HeroCarousel from '../components/HeroCarousel';
 
